fce08f346e50091edfa603ec27a74fe473dff935
ryutaroM <arcticmonkey326@gmail.com>
2024-08-21 23:57:52 +0900

    done up to errors

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -63,3 +63,21 @@ fn read_username_from_file() -> Result<String, io::Error> {
     File::open("hello.txt")?.read_to_string(&mut s)?;
     Ok(s)
 }
+
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
