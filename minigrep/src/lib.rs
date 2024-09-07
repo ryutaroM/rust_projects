33d08d6d29a0db026774a219b7a5b129ec5eb103
ryutaroM <arcticmonkey326@gmail.com>
2024-09-07 23:44:35 +0900

    done up to chap12.4.1

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,56 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments!");
+        }
+
+        Ok(Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        )
+    }
+}
